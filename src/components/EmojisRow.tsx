6251f3fee0aec82193dd9fb7bc1e85070b531f5e
Facundo <floresfacundo68@gmail.com>
2022-12-31 19:53:00 -0300

    add EmojisRow

---
+++ src/components/EmojisRow.tsx
@@ -0,0 +1,19 @@
+import { Quizz } from '@/interfaces/quizz';
+
+type Props = {
+  quizz: Quizz;
+};
+
+const EmojisRow: React.FC<Props> = ({ quizz }) => {
+  return (
+    <div className="mt-4 flex flex-row space-x-2">
+      {quizz.emojis.map((emoji) => (
+        <div key={emoji} className="rounded-lg bg-gray-200 p-2">
+          <span className="text-2xl">{emoji}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EmojisRow;
