81f8f62d89184ed03c279ce482aa41a66ec56975
Facundo <floresfacundo68@gmail.com>
2023-01-15 00:20:09 -0300

    add more quizzes

--- src/utils/getQuizzes.ts
+++ src/utils/getQuizzes.ts
@@ -1,14 +1,46 @@
-import { Quiz } from '@/interfaces/quizz';
+import { Quiz } from '@/interfaces/interfaces';
 
 export const getQuizzes = (): Quiz[] => {
   return [
+    {
+      answer: 'MESSI',
+      emojis: ['🐐', '🔟'],
+    },
     {
       answer: 'MBAPPE',
       emojis: ['🗼', '🐢'],
     },
     {
-      answer: 'MESSI',
-      emojis: ['🐐', '🔟'],
+      answer: 'DI MARIA',
+      emojis: ['😇', '🍜'],
+    },
+    {
+      answer: 'GRIEZZMANN',
+      emojis: ['🧉', '🤴'],
+    },
+    {
+      answer: 'RONALDO',
+      emojis: ['🐛', '💪🏻'],
+    },
+    {
+      answer: 'DYBALA',
+      emojis: ['💎', '👉'],
+    },
+    {
+      answer: 'SUAREZ',
+      emojis: ['🧛🏻‍♂️', '👉'],
+    },
+    {
+      answer: 'ZLATAN',
+      emojis: ['🥋', '👃🏻'],
+    },
+    {
+      answer: 'BENZEMA',
+      emojis: ['😺', '🗼'],
+    },
+    {
+      answer: 'NEYMAR',
+      emojis: ['🎭', '🥳'],
     },
   ];
 };
